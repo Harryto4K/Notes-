bc15623ef10a5e34f361e0da7c640e615ac5daba
Harryto4K <harryyadier@gmail.com>
2023-04-14 00:30:14 -0400

    added a new feature: vertical-horizontal button

--- index.js
+++ index.js
@@ -21,7 +21,32 @@ delNote.onclick = function() {
     console.log('delnote');
 }
 
+let alignButton = document.getElementById('align-button'); 
 
+let alignButtonFunc = function() {
+    let notesContainer = document.getElementById('notes-container'); 
+    let notes = document.getElementsByClassName('note'); 
+
+    if (alignButton.innerHTML === 'Vertical') {
+        notesContainer.style.flexDirection = 'column'; 
+        for (let x=0; x<notes.length; x++) {
+            notes[x].style.width = '20%'; 
+            notes[x].style.margin = '0.3% 0';
+            notes[x].style.flexGrow = 0; 
+        }
+        alignButton.innerHTML = 'Horizontal';
+    } else if (alignButton.innerHTML === 'Horizontal') {
+        notesContainer.style.flexDirection = 'row'; 
+        for (let y=0; y<notes.length; y++) {
+            notes[y].style.width = '24.4%';
+            notes[y].style.margin = '0 0.3%';
+            notes[y].style.flexGrow = 0;
+        }
+        alignButton.innerHTML = 'Vertical';
+    }
+}
+
+alignButton.onclick = alignButtonFunc; 
 
 
 
